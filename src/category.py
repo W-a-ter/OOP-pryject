cea61db9b368cc7dafa3104687a45ec443b35e6e
verba4182 <verba4182@mail.ru>
2024-08-03 23:26:16 +0300

    14.2 03/08

--- src/category.py
+++ src/category.py
@@ -2,10 +2,6 @@ class Category:
     counter_products = 0
     counter_category = 0
 
-    name: str
-    description: str
-    products: list
-
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
@@ -13,3 +9,20 @@ class Category:
 
         self.counter_products += len(products)
         self.counter_category += 1
+
+    def add_product(self, new_products):
+        self.__products.append(new_products)
+        self.counter_products += 1
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += (f"{product.name}, "
+                            f"{product.price} руб. Остаток: "
+                            f"{product.quantity} шт.\n")
+        return product_str
+
+    @property
+    def list_products(self):
+        return self.__products
--- tests/test_category.py
+++ tests/test_category.py
@@ -1,7 +1,15 @@
-from src.category import Category
-
-
 def test_init(category1):
     assert category1.name == "Смартфоны"
     assert (category1.description == "Смартфоны, как средство не только коммуникации, "
                                      "но и получения дополнительных функций для удобства жизни")
+
+
+def test_category_product_property(category1):
+    assert category1.products == ('Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n'
+                                  'Iphone 15, 210000.0 руб. Остаток: 8 шт.\n')
+
+
+def test_test_category_product_setter(category1, product1):
+    assert len(category1.list_products) == 2
+    category1.add_product(product1)
+    assert len(category1.list_products) == 3
